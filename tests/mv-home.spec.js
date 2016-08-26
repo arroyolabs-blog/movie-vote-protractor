b94c7dbfd75c4ef12ad3a7ecfdeae0e3825f9088
Andy Armstrong <andyarmstro@gmail.com>
2016-08-25 18:05:14 -0700

    updated tests

--- tests/mv-home.spec.js
+++ tests/mv-home.spec.js
@@ -4,7 +4,7 @@ describe('Movie Vote App - Home Page', function() {
     // tun off the synchronization detection that would
     // prevent stuff from loading
     beforeEach(function() {
-        return dv.ignoreSynchronization = false;
+        dv.ignoreSynchronization = true;
     });
 
     // helper method to load the home page and return a promise
@@ -12,20 +12,39 @@ describe('Movie Vote App - Home Page', function() {
         return dv.get('http://localhost:8000/');
     }
 
+    // helper for taking screenshots
+    function writeScreenShot(data, filename) {
+        var stream = fs.createWriteStream(filename);
+        stream.write(new Buffer(data, 'base64'));
+        stream.end();
+    }
+
+    //afterEach(function() {
+    //    var passed = jasmine.getEnv().currentSpec.results().passed();
+    //    if (!passed) {
+    //        var fs = require('file-system');
+    //        dv.takeScreenshot().then((png) => {
+    //              writeScreenShot(png, 'foo.png');
+    //        });
+    //    }
+    //});
+
     it('should see rendered page elements', function() {
         loadHomePage().then(function () {
 
             // check the title exists
             expect(dv.getTitle()).toEqual('Movie Vote');
 
-            // check other elements here...
-
+            var foo = dv.findElements(By.xpath('//*[@id="votes"]')).then(function(el) { 
+                expect(el.length).toEqual(1); 
+            });
         });
     });
 
     it('should increment vote count when clicking upvote', function() {
         loadHomePage().then(function () {
-            expect(dv.findElement(by.id('votes')).getText()).toEqual("0");
+            var votes = dv.findElement(by.id('votes'));
+            expect(votes.getText()).toEqual("0");
             dv.findElement(by.id('upvote')).click();
             expect(dv.findElement(by.id('votes')).getText()).toEqual("1");
 
